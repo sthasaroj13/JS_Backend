950f90becfaf1968fa8b2cb450cea4cf64a1db69
sthasaroj13 <sarojshrestha651@gmail.com>
2024-02-24 09:37:07 +0545

    utis code  (api error handling and api response)

--- src/index.js
+++ src/index.js
@@ -7,10 +7,32 @@ import connectDB from "./db/index.js";
 dotenv.config({
     path:'./env'
 })
+import app from './app.js';
 
 
 
 connectDB()
+.then(app.listen(process.env.PORT || 8000,()=>{
+    console.log(`server is running ${process.env.PORT}`);
+
+}))
+.catch((err)=>{
+    console.log('mongoDb connection fail',err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /* this is first approach to connect data base
 
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,11 @@
+class ApiResponse{
+    constructor(statusCode,data,message='Success'){
+        this.statusCode= statusCode
+        this.data=data
+        this.message= message
+        this.success = statusCode <400
+
+    }
+}
+// const apires = new ApiResponse(200)
+// console.log(apires.statusCode);

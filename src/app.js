1e4d57104e14261ece5cf4478e54c2e5ccdf157c
sthasaroj13 <sarojshrestha651@gmail.com>
2024-02-27 12:38:34 +0545

    routes and controllers setup done

--- src/app.js
+++ src/app.js
@@ -16,4 +16,11 @@ app.use(express.static('public'))
 
 app.use(cookieParser())
 
+
+//routes immport
+import userRouter from './routes/user.routes.js'
+
+// routes decleration
+
+app.use('/api/v1/users',userRouter)
 export default  app
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 
 const asyncHandler = (reqHandler) =>(req,res,next)=>{
-    Promise.resolve(reqHandler(req,res,next))
+   Promise.resolve(reqHandler(req,res,next))
     .catch((err)=>next(err))
 
 }
